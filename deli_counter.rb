410ebccab79392c34f095fd91a88ce07d8060bfc
MC Simoes <mariacristina.simoes@gmail.com>
2019-01-05 03:38:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.length == 0 
+  puts "The line is currently empty."
+end
+else
+  counter = 1
+  num_array = []
+  katz_deli.each do |name|
+    num_array.push("#{counter}. #{name}")
+    counter++
+  end
+  puts "The line is currently" num_array
+end
+end
